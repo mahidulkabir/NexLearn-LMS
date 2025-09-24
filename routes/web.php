06c759bdc5bc1fa9e89e18fbff321b63cacdda9f
mahidulkabir <mahidulkabir8@gmail.com>
2025-09-24 07:06:47 +0600

    added login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\controllers\backend\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+// Admin Login Routes
+Route::get ('/admin/login',[AdminController::class,'login'])->name('admin.login');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
